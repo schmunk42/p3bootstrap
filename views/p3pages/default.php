9ad15d7899988c8463ba6e8fe171249f196f21f9
Marc <m.mautz@herzogkommunikation.de>
2012-04-23 17:04:13 +0200

    reverted cms page

--- views/p3pages/default.php
+++ views/p3pages/default.php
@@ -5,33 +5,16 @@ $this->breadcrumbs = array(
 );
 ?>
 <div class="row">
-    <div class="span4">
-        <?php #$this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'left')) ?>
-        <a href="/<?php echo Yii::app()->language ?>">
-            <img class="riad-du-rabbin-logo" src="<?php echo Yii::app()->baseUrl."/images/riad-du-rabbin-logo.png" ?>" width="246" height="147" />
-        </a>
-    </div>
-    <div class="span8">
-        <div class="navi">
-            <?php
-            Yii::import('p3pages.modules.*');
+	<div class="span12">
+		<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'top', 'varyByRequestParam' => P3Page::PAGE_ID_KEY)) ?>
+	</div>
+</div>
 
-            $rootNode = P3Page::model()->findByAttributes(array('layout' => '_BootMenu'));
-            $this->widget('ext.crisu83.yii-bootstrap.widgets.BootNavbar', array(
-                'collapse' => false,
-                'brand'=> false,
-                'items' => array(
-                    array(
-                        'class' => 'bootstrap.widgets.BootMenu',
-                        'items' => P3Page::getMenuItems($rootNode)
-                    ),
-                    ))
-            );
-            ?>
-        </div>
-        <div class="clearfix"></div>
-        <div class="main-content">
-            <?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main', 'varyByRequestParam' => P3Page::PAGE_ID_KEY)) ?>
-        </div>
-    </div>
+<div class="row">
+	<div class="span8">	
+		<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'main', 'varyByRequestParam' => P3Page::PAGE_ID_KEY)) ?>
+	</div>
+	<div class="span4">
+		<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'sidebar', 'varyByRequestParam' => P3Page::PAGE_ID_KEY)) ?>
+	</div>
 </div>
