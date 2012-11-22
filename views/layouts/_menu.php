0faebdc03157339a82284a9a6b48f24db595d0e6
Tobias Munk <schmunk@usrbin.de>
2012-11-23 00:45:28 +0100

    added menu icons, fixed typo

--- views/layouts/_menu.php
+++ views/layouts/_menu.php
@@ -38,22 +38,22 @@ $this->widget('TbNavbar', array(
                                        'class' => 'TbMenu',
                                        'htmlOptions' => array('class' => 'pull-right'),
                                        'items' => array(
-                                           array('label' => 'Phundament 3', 'url' => '#', 'visible' => !Yii::app()->user->isGuest, 'items' => array(
+                                           array('label' => 'Manage', 'url' => '#', 'visible' => !Yii::app()->user->isGuest, 'icon'=>'cog white', 'items' => array(
                                                array('label' => 'Upload Media', 'url' => array('/p3media/import/upload'), 'visible' => Yii::app()->user->checkAccess('P3media.Import.*')),
                                                array('label' => 'Edit Page Translation', 'url' => array('/p3pages/p3PageTranslation/update', 'id' => (P3Page::getActivePage()->getTranslationModel()) ? P3Page::getActivePage()->getTranslationModel()->id : null), 'visible' => Yii::app()->user->checkAccess('P3pages.P3PageTranslation.*') && P3Page::getActivePage()->getTranslationModel()),
                                                array('label' => 'Create Page Translation', 'url' => array('/p3pages/p3PageTranslation/create', 'P3PageTranslation' => array('p3_page_id' => P3Page::getActivePage()->id)), 'visible' => Yii::app()->user->checkAccess('P3pages.P3PageTranslation.*') && !P3Page::getActivePage()->getTranslationModel() && !P3Page::getActivePage()->isNewRecord),
                                                '---',
-                                               array('label' => 'Media', 'url' => array('/p3media'), 'visible' => Yii::app()->user->checkAccess('P3media.Import.*')),
-                                               array('label' => 'Pages', 'url' => array('/p3pages'), 'visible' => Yii::app()->user->checkAccess('P3media.Import.*')),
-                                               array('label' => 'Widgets', 'url' => array('/p3widgets'), 'visible' => Yii::app()->user->checkAccess('P3media.Import.*')),
+                                               array('label' => 'Media', 'url' => array('/p3media'), 'visible' => Yii::app()->user->checkAccess('P3media.Default.*')),
+                                               array('label' => 'Pages', 'url' => array('/p3pages'), 'visible' => Yii::app()->user->checkAccess('P3pages.Default.*')),
+                                               array('label' => 'Widgets', 'url' => array('/p3widgets'), 'visible' => Yii::app()->user->checkAccess('P3widgets.Default.*')),
                                                '---',
                                                array('label' => 'Users', 'url' => array('/user'), 'visible' => !Yii::app()->user->isGuest),
                                                array('label' => 'Rights', 'url' => array('/rights'), 'visible' => Yii::app()->user->checkAccess('Admin')),
                                                array('label' => 'Administration', 'url' => array('/p3admin'), 'visible' => Yii::app()->user->checkAccess('Admin')),
                                                '---',
-                                               array('label' => 'Visit Phundament Website', 'url' => 'http://phundament.com'),
+                                               array('label' => 'Visit Phundament 3 Website', 'url' => 'http://phundament.com'),
                                            )),
-                                           array('label' => ucfirst(Yii::app()->user->name), 'visible' => !Yii::app()->user->isGuest, 'items' => array(
+                                           array('label' => ucfirst(Yii::app()->user->name), 'visible' => !Yii::app()->user->isGuest, 'icon'=>Yii::app()->user->isSuperuser?'warning-sign white':'user white', 'items' => array(
                                                array('label' => 'User Settings'),
                                                array('label' => 'Profile', 'url' => array('/user/profile'), 'visible' => !Yii::app()->user->isGuest),
                                                '---',
