a8a66cca5932d263253923b77e52c2122a7536c6
Tobias Munk <schmunk@usrbin.de>
2013-02-04 12:44:46 +0100

    added hotfix for navbar on iOS

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,49 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
-        <meta name="description" content="">
-        <meta name="author" content="">
+<head>
+    <meta charset="utf-8">
+    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        <style type="text/css">
-            @media (min-width: 989px) {
-                body {
-                    margin-top: 60px;
-                    padding-bottom: 40px;
-                }
+    <style type="text/css">
+        @media (min-width: 989px) {
+            body {
+                margin-top: 60px;
+                padding-bottom: 40px;
             }
-        </style>
+        }
+        /* TODO: temp fix from https://github.com/twitter/bootstrap/issues/4550#issuecomment-10398831 */
+        @media (max-width: 980px) {
+            .dropdown ul.dropdown-menu {
+                display: block;
+            }
+        }
+    </style>
 
-        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-        <!--[if lt IE 9]>
-          <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-        <?php
-        $cs = Yii::app()->getClientScript();
-        $cs->registerMetaTag('width=device-width, initial-scale=1.0', 'viewport');
-        $cs->registerLinkTag('shortcut icon', NULL, Yii::app()->theme->baseUrl.'/img/favicon.ico', NULL, NULL);
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <?php
+    $cs = Yii::app()->getClientScript();
+    $cs->registerMetaTag('width=device-width, initial-scale=1.0', 'viewport');
+    $cs->registerLinkTag('shortcut icon', NULL, Yii::app()->theme->baseUrl . '/img/favicon.ico', NULL, NULL);
 
-        $cs->registerCssFile(Yii::app()->theme->baseUrl.'/css/p3.css');
-        // if you've installed crisu83/yii-less as a application component, comment line above and uncomment line below
-        //Yii::app()->less->register();
+    $cs->registerCssFile(Yii::app()->theme->baseUrl . '/css/p3.css');
+    // if you've installed crisu83/yii-less as a application component, comment line above and uncomment line below
+    //Yii::app()->less->register();
 
-        //Yii::app()->bootstrap->register(); #yii-bootstrap 2.x
-        ?>
-    </head>
+    //Yii::app()->bootstrap->register(); #yii-bootstrap 2.x
+    ?>
+</head>
 
-    <body>
+<body>
 
-    <?php $this->renderFile(Yii::getPathOfAlias('application.themes.frontend.views.layouts').DIRECTORY_SEPARATOR.'_menu.php') ?>
+<?php $this->renderFile(Yii::getPathOfAlias('application.themes.frontend.views.layouts') . DIRECTORY_SEPARATOR . '_menu.php') ?>
 
-        <div class="container">
-            <div class="subwrapper">
-                <?php echo $content; ?>
-            </div>
-            <hr>
-            <footer>
-                <?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'footer')) ?>
-            </footer>
-        </div> <!-- /container -->
-    </body>
+<div class="container">
+    <div class="subwrapper">
+        <?php echo $content; ?>
+    </div>
+    <hr>
+    <footer>
+        <?php $this->widget('p3widgets.components.P3WidgetContainer', array('id' => 'footer')) ?>
+    </footer>
+</div>
+<!-- /container -->
+</body>
 </html>
